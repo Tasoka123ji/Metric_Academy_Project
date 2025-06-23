45c4b7c6226cb1570df09ca470bff8ad17754958
Tasoka123ji <antonyank36@gmail.com>
2025-06-23 18:39:22 +0400

    fix: main.py

--- app.py
+++ app.py
@@ -6,3 +6,6 @@ st.set_page_config(page_title="Hi My Metric Team", page_icon="📊")
 st.title("📣 Hi Vika, Nane es mer projecti url-na linelu skzbnakan u mer cragrery vor grenq stexov erevaluya......")
 st.subheader("We can start together ")
 
+
+
+# for testing 
