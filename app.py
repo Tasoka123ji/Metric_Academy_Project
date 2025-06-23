e26e608f6f473b56be36afa08b275f7da083e731
Tasoka123ji <antonyank36@gmail.com>
2025-06-23 18:10:52 +0400

    fix: main.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import streamlit as st
 st.set_page_config(page_title="Hi My Metric Team", page_icon="📊")
 
 st.title("📣 Hi Vika, Nane es mer projecti url-na linelu skzbnakan u mer cragrery vor grenq stexov erevaluya......")
-st.subheader("We can start together 🚀")
+st.subheader("We can start together ")
 
